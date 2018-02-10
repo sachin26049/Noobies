06ff690e5815d9af20ef53d84ab4377916acf9b6
Sachin Choudhary <sachin26049@gmail.com>
2018-02-10 15:39:09 +0530

    init

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+// Connect To Database
+mongoose.connect(config.database);
+
+// On Connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database '+config.database);
+});
+
+// On Error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error: '+err);
+});
+
+const app = express();
+
+const users = require('./routes/users');
+const menu=require('./routes/Menu');
+const orders=require('./routes/orders');
+// Port Number
+const port = 3000;
+
+// CORS Middleware
+app.use(cors());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
