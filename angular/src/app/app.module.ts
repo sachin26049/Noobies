695ed7b3dd894f9c78544bb9932c00631aea7ae5
SANKET DOSHI <sdoshi579@gmail.com>
2018-02-10 19:41:35 +0530

    hello1

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -7,24 +7,43 @@ import {HttpClientModule} from '@angular/common/http';
 import {RouterModule, Routes} from '@angular/router';
 import {FlashMessagesModule} from 'angular2-flash-messages';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {NgcFloatButtonModule} from 'ngc-float-button';
+//import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+//import {MatTabsModule} from '@angular/material/tabs';
+//import {MatExpansionModule} from '@angular/material/expansion';
+//import {NgcFloatButtonModule} from 'ngc-float-button';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
+import {AuthService} from './services/auth.service';
+import {ValidateService} from './services/validate.service';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  {path: '' , component: HomeComponent},
+  {path: 'login' , component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    FlashMessagesModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    NoopAnimationsModule,
+    ],
+  providers: [ValidateService,AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
